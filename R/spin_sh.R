3ab4f509e6e589f1d244e85735843e11c84a31c6
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2019-10-14 15:32:09 +0200

    Added first version of spin_sh script including test

--- R/spin_sh.R
+++ R/spin_sh.R
@@ -1,143 +1,75 @@
-#' Spin a bash script (goat's hair) into Rmd (wool)
+#' Spin a bash script into Rmd
 #'
 #' @description
 #' This function is derived from \code{knitr::spin()} for R-scripts and
 #' is adapted to do a similar conversion of bash scripts into Rmd files.
-#' This function (\code{spin_sh}) does not handle inline bash statements
-#' which are bash commands inside a text. The only format that is
-#' considered is Rmd.
+#' The conversion is done in a simple way and almost no features of the
+#' original are provided. This is just a very crude way to have a look
+#' at documentation of a shell script in a nicer format such as html
+#' or pdf.
 #'
-#' @param hair path to the bash scirpt
-#' @param knit
-#' @param report
-#' @param text
-#' @param envir
-#' @param doc
-#' @param comment  pair of pattern defining a comment
-#' @param precious should output source document be kept
+#' @param ps_sh_hair path to the bash scirpt
+#' @param ps_out_rmd output Rmd file
 #'
-#' @return
-#' @export
+#' @export spin_sh
 #'
 #' @examples
-spin_sh <- function (hair, knit = TRUE, report = TRUE, text = NULL, envir = parent.frame(),
-          doc      = "^#+'[ ]?",
-          comment  = c("^[# ]*/[*]", "^.*[*]/ *$"),
-          precious = !knit && is.null(text))
-{
-  format = 'Rmd'
-  x = if (nosrc <- is.null(text)){
-    read_utf8(hair)
-  } else {
-    split_lines(text)
+#' s_test_script <- system.file('extdata', 'test_script.sh', package = 'rmdhelp')
+#' s_test_out <- file.path(tempdir(), 'test_script.Rmd')
+#' spin_sh(ps_sh_hair = s_test_script, ps_out_rmd = s_test_out)
+#' unlink(s_test_out)
+spin_sh <- function (ps_sh_hair, ps_out_rmd = paste0(fs::path_ext_remove(ps_sh_hair), ".Rmd")){
+  if (!file.exists(ps_sh_hair))
+    stop(" * ERROR cannot find input script: ", ps_sh_hair)
+
+  # read the script content
+  vec_bsrc <- readLines(con = ps_sh_hair)
+
+  #' In case the script starts with the path to bash remove it.
+  vec_out_src <- vec_bsrc
+  if (vec_out_src[1] == '#!/bin/bash')
+    vec_out_src <- vec_out_src[-1]
+
+  #' Insert the chunk borders
+  #' There are two types of chunks:
+  #'
+  #' 1. text chunks where text chunks are all lines that start with "#'"
+  #' 2. code chunks are all lines that are not text chunks.
+  #'
+  #' From the definitions given above it seams easier to start with text chunks
+  vec_txt_chunks <- grep("^#\\'", vec_out_src)
+
+
+  #' Start with the beginning of a code chunk
+  vec_code_chunk_start_pos <- grep(pattern = '^#\\+', vec_out_src)
+
+  #' End of code chunks
+  vec_txt_chunks <- c(vec_txt_chunks, length(vec_out_src))
+  vec_code_chunk_end_pos <- sapply(1:length(vec_code_chunk_start_pos),
+                                   function(x) vec_txt_chunks[which(vec_code_chunk_start_pos[x]<vec_txt_chunks)[1]]-1,
+                                   USE.NAMES = FALSE)
+  if (length(vec_code_chunk_start_pos) != length(vec_code_chunk_end_pos))
+    stop("ERROR: number of code start pos not the same as code end pos")
+
+
+  #' ## Replacements to get to a rmd document
+  #' The code chunks are augmented with the tiks and engine specs
+  vec_out_src[vec_code_chunk_start_pos] <- gsub(pattern = '^#\\+(.*)$', replacement = '```{bash,\\1}', vec_out_src[vec_code_chunk_start_pos])
+
+  #' The code chunk ends are symbolised with three tiks which are added at each beginning of the line
+  for (i in seq_along(vec_code_chunk_end_pos)){
+    if (vec_out_src[vec_code_chunk_end_pos[i]] == ""){
+      vec_out_src[vec_code_chunk_end_pos[i]] <- "```\n"
+    } else {
+      vec_out_src <- c(vec_out_src[1:(vec_code_chunk_end_pos[i])], "```\n", vec_out_src[(vec_code_chunk_end_pos[i]+1):length(vec_out_src)])
+    }
   }
 
-  stopifnot(length(comment) == 2L)
-  c1 = grep(comment[1], x)
-  c2 = grep(comment[2], x)
-  if (length(c1) != length(c2))
-    stop("comments must be put in pairs of start and end delimiters")
-  if (length(c1))
-    x = x[-unique(unlist(mapply(seq, c1, c2, SIMPLIFY = FALSE)))]
-  # parsed_data = utils::getParseData(parse(text = x, keep.source = TRUE))
-  # is_matchable = seq_along(x) %in% unique(parsed_data[parsed_data$col1 ==
-                                                        # 1, "line1"])
-  p = if (identical(tolower(format), "rmd")){
-    .fmt.rmd(x)
-  } else {
-    stop(" * Unknown Format: ", format)
-  }
-  # r = rle((is_matchable & grepl(doc, x)) | i)
-  r = rle(grepl(doc, x))
+  #' Remove all doxygen comment signs from text
+  vec_out_src <- gsub(pattern = "^#\\'[ ]*", replacement = "", vec_out_src )
 
-  n = length(r$lengths)
-  txt = vector("list", n)
-  idx = c(0L, cumsum(r$lengths))
-  p1 = gsub("\\{", "\\\\{", paste0("^", p[1L], ".*", p[2L],
-                                   "$"))
-  for (i in seq_len(n)) {
-    block = x[seq(idx[i] + 1L, idx[i + 1])]
-    txt[[i]] = if (r$values[i]) {
-      sub(doc, "", block)
-    }
-    else {
-      block = strip_white(block) # error: cannot be found
-      if (!length(block))
-        next
-      if (length(opt <- grep(rc <- "^(#|--)+(\\+|-| ----+| @knitr)",
-                             block))) {
-        block[opt] = paste0(p[1L], gsub(paste0(rc, "\\s*|-*\\s*$"),
-                                        "", block[opt]), p[2L])
-        if (any(opt > 1)) {
-          j = opt[opt > 1]
-          block[j] = paste(p[3L], block[j], sep = "\n")
-        }
-      }
-      if (!grepl(p1, block[1L])) {
-        block = c(paste0(p[1L], p[2L]), block)
-      }
-      c("", block, p[3L], "")
-    }
-  }
-  txt = unlist(txt)
-  if (report && format %in% c("Rnw", "Rtex") && !grepl("^\\s*\\\\documentclass",
-                                                       txt)) {
-    txt = c("\\documentclass{article}", "\\begin{document}",
-            txt, "\\end{document}")
-  }
-  if (nosrc) {
-    outsrc = with_ext(hair, format)
-    xfun::write_utf8(txt, outsrc)
-    txt = NULL
-  }
-  else outsrc = NULL
-  if (!knit)
-    return(txt %n% outsrc)
-  out = if (report) {
-    if (format == "Rmd") {
-      knit2html(outsrc, text = txt, envir = envir, encoding = "UTF-8")
-    }
-    else if (!is.null(outsrc) && (format %in% c("Rnw", "Rtex"))) {
-      knit2pdf(outsrc, envir = envir)
-    }
-  }
-  else knit(outsrc, text = txt, envir = envir, encoding = "UTF-8")
-  if (!precious && !is.null(outsrc))
-    file.remove(outsrc)
-  invisible(out)
+  cat(paste0(vec_out_src, collapse = "\n"), "\n", file = ps_out_rmd)
+
+  return(invisible(NULL))
 }
-# <bytecode: 0x7f85d1290f70>
-# <environment: namespace:knitr>
 
-
-#' Split a string at newline into vector
-#'
-#' @param x original input string
-#'
-#' @return vector of lines
-split_lines <- function (x) {
-  if (length(grep("\n", x)) == 0L)
-    return(x)
-  x = gsub("\n$", "\n\n", x)
-  x[x == ""] = "\n"
-  unlist(strsplit(x, "\n"))
-}
-# <bytecode: 0x7f85d499adf8>
-# <environment: namespace:knitr>
-
-.fmt.rmd <- function (x)
-{
-  x = paste(x, collapse = "\n")
-  l = attr(gregexpr("`+", x)[[1]], "match.length")
-  l = max(l, 0)
-  if (length(l) > 0) {
-    i = highr:::spaces(l + 1, "`")
-    b = highr:::spaces(max(l + 1, 3), "`")
-  }  else {
-    i = "`"
-    b = "```"
-  }
-  c(paste0(b, "{bash "), "}", b, paste0(i, "bash \\1 ", i))
-}
-# <bytecode: 0x7fd6290c04a8>
-#   <environment: namespace:knitr>
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(rmdhelp)
+
+test_check("rmdhelp")
