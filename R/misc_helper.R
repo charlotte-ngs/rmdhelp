6c77b84e9b7dba4e3881071286e022d58dd09e97
charlotte-ngs <ngs.charlotte@gmail.com>
2021-03-19 12:03:15 +0100

    Implementation of clean up function

--- R/misc_helper.R
+++ R/misc_helper.R
@@ -97,3 +97,55 @@ get_latest_change <- function(ps_msg = "Latest Changes"){
                       Sys.time(), " (", Sys.info()[["user"]], ")_", collapse = "")
   return(st_result)
 }
+
+## --- Clean Rmarkdown Directory ----------------------------------------------
+#'
+#' @title Clean RMarkdown Directory
+#'
+#' @description
+#' RMarkdown docments are often placed in separate directories to avoid cluttering
+#' a given directory with output files of different source files. Output files
+#' are almost never versioned, hence it is useful to have a function to remove
+#' all RMarkdown output files. This function helps to clean up directories from
+#' RMarkdown output files.
+#'
+#' @examples
+#' \dontrun{
+#' clean_rmd_dir(ps_rmd_dir = 'rmd')
+#' }
+#'
+#' @param ps_rmd_dir RMarkdown source directory
+#'
+#' @export clean_rmd_dir
+clean_rmd_dir <- function(ps_rmd_dir){
+  # vector of elements to be cleaned
+  vec_element_path <- list.files(path = ps_rmd_dir, pattern = '[^Rmd]$', full.names = 'TRUE')
+  vec_element_files <- basename(vec_element_path)
+  # choice for user
+  cat("\n * List of elements to be deleted in directory: ", ps_rmd_dir, "\n", paste0(vec_element_files, collapse = ', '), '\n')
+  cat("\n * Select on of the following options\n\n")
+  cat("\t 1 Delete all files\n")
+  cat("\t 2 Delete selected files\n")
+  cat("\t 3 Delete no files\n")
+  answer <- readline(prompt = " * Specifiy your input: ")
+  # action according to users answer
+  if (answer == "1"){
+    cat ("  ==> Deleting all of the following files: ", paste0(vec_element_files, collapse = ', '), "\n")
+    fs::file_delete(path = vec_element_path)
+  } else if(answer == "2"){
+    cat ("  ==> Delete selected files from: ", paste0(vec_element_files, collapse = ', '), "\n")
+    for (p in vec_element_path){
+      sel_answer <- readline(prompt = paste0(" ** Delete file ", p, " [y/n] ", collapse = ''))
+      if (sel_answer == 'y'){
+        cat("    ==> Delete file: ", p, "\n")
+        fs::file_delete(path = p)
+      }
+    }
+
+  } else {
+    cat ("  ==> Delete no files\n")
+  }
+
+  return(invisible(NULL))
+}
+
