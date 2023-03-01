ff0f50c7ddc3e815aed666a108f69cc87dd7efc1
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2023-03-01 14:24:27 +0100

    Added new template for vignette to be used with rmarkdown

--- R/vignette_to_dir.R
+++ R/vignette_to_dir.R
@@ -48,9 +48,13 @@ vig_to_dir <- function(pvec_rmd_doc = list.files(file.path(here::here(), "vignet
 #'
 #' @param ps_name Base of vignette file name
 #' @param ps_title Title of vignette
+#' @param ps_template Name of template to be used
+#' @param pb_open Directly open the vignette
 #'
 #' @import fs
-#' @import here
+#' @import usethis
+#' @import glue
+#' @import rmarkdown
 #' @export use_vignette_in_dir
 #'
 #' @examples
@@ -58,16 +62,33 @@ vig_to_dir <- function(pvec_rmd_doc = list.files(file.path(here::here(), "vignet
 #' use_vignette_in_dir("example-vignette", "Example Vignette")
 #' }
 use_vignette_in_dir <- function(ps_name,
-                                ps_title = fs::path_ext_remove(ps_name)){
+                                ps_title    = fs::path_ext_remove(ps_name),
+                                ps_template = "vignette",
+                                ps_pkg      = "rmdhelp",
+                                pb_open     = FALSE){
   s_name <- basename(fs::path_ext_remove(ps_name))
   # convert name of vignette to a path
-  if (basename(s_name) == s_name){
-    s_vig_dir <- file.path(here::here(), "vignettes")
+  if (basename(ps_name) == s_name){
+    s_vig_dir <- file.path("vignettes", s_name)
   } else {
     s_vig_dir <- dirname(ps_name)
   }
-  # create vignette
-  usethis::use_vignette(name = s_name, title = ps_title)
-  # move vignette to sub directory
-  vig_to_dir(s_name, ps_trg_dir = s_vig_dir)
+  s_vig_path <- file.path(s_vig_dir, s_name)
+  # create vignette document using rmarkdown::draft
+  rmarkdown::draft(s_vig_path, template = ps_template, package = ps_pkg, edit = FALSE)
+  # replace template
+  con_vig <- file(s_vig_path)
+  vec_vig <- readLines(con_vig)
+  close(con_vig)
+  s_result_vig <- glue::glue(paste0(vec_vig, collapse = "\n"),
+                             vignette_title = ps_title,
+                             braced_title = ps_title,
+                             .open = "{{{",
+                             .close = "}}}")
+  # write output
+  cat(s_result_vig, "\n", sep = "", file = s_vig_path)
+  # open file for editing, if specified
+  if (pb_open) usethis::edit_file(s_vig_path)
+  # return invisible
+  invisible()
 }
